45ecbecc8800e0f31892c3c274adb004eb0c8f9c
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-24 16:47:28 +0300

    added django corsheaders to the project, updated the requirements.txt. configured cors in the settings.py

--- bus_reservation/settings.py
+++ bus_reservation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -113,6 +116,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+# CORS CONFIG
+CORS_ORIGIN_ALLOWED_ALL = True
+ALLOWED_HOSTS =['*']
+
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:4200/'
+# ]
 
 
 # Static files (CSS, JavaScript, Images)
