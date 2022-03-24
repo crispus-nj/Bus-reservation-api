3319cead4aecd3a39bd0858b5c65caae18ad6189
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-24 14:36:40 +0300

    created models for necessary for the project

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Bus(models.Model):
+    bus_number = models.CharField(max_length=10)
+    departure_city = models.CharField(max_length=50)
+    arrival_city = models.CharField(max_length=50)
+    estimated_time_of_departure = models.TimeField()
+
+    def __str__(self):
+        return self.bus_number
+
+class Passenger(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.username
+
+class Reservationn(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bus.bus_number
